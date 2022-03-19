4335179aebf6ee13e24bf15dd84d17c97b14a06f
codecamjam <camerontaghlabi@gmail.com>
2022-03-18 20:44:49 -0400

    l36: setting up css modules

--- section-3-TS-with-react/src/components/Pizza.tsx
+++ section-3-TS-with-react/src/components/Pizza.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import PizzaCss from './Pizza.module.css';
 
 interface Pizza {
   id: number;
@@ -13,7 +14,7 @@ interface Props {
 
 const Pizza: React.FC<Props> = ({ pizza }) => {
   return (
-    <li>
+    <li className={PizzaCss.container}>
       <h2>{pizza.name}</h2>
       <p>{pizza.description}</p>
       <p>{pizza.price}</p>
