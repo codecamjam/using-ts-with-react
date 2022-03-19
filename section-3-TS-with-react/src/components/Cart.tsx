f69f3ef82507ffa79e1ffdb6df277c14e9d98c0d
codecamjam <camerontaghlabi@gmail.com>
2022-03-18 21:42:27 -0400

    l39: handling events using react

--- section-3-TS-with-react/src/components/Cart.tsx
+++ section-3-TS-with-react/src/components/Cart.tsx
@@ -16,15 +16,21 @@ export default class Cart extends React.Component<Props, State> {
     };
   }
 
+  handleClick = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+    console.log(e.target);
+    // if ((e.target as HTMLElement).nodeName === 'SPAN') {
+    //   (e.target as HTMLElement).
+    // }
+    this.setState((prevState) => ({ isOpen: !prevState.isOpen }));
+  };
+
   render() {
     return (
       <div className={CartCSS.cartContainer}>
         <button
           className={CartCSS.button}
           type="button"
-          onClick={() => {
-            this.setState((prevState) => ({ isOpen: !prevState.isOpen }));
-          }}
+          onClick={this.handleClick}
         >
           <FiShoppingCart />
           <span>2 pizza(s)</span>
